121e364244862849f3d4630072e75321473e1712
fuchiwara <Nichakorn_ronram@kkumail.com>
2025-03-19 20:39:15 +0700

    Update game.js

--- game.js
+++ game.js
@@ -3,32 +3,6 @@ const config = {
   width: 800,
   height: 600,
   scene: {
-    preload: preload,
-    create: create,
-    update: update
-  }
-};
-
-const game = new Phaser.Game(config);
-
-function preload() {
-  // โหลดไฟล์เกม เช่น
-  this.load.image('background', 'assets/cyber_background.png');
-}
-
-function create() {
-  // แสดงฉากพื้นหลัง
-  this.add.image(400, 300, 'background');
-}
-
-function update() {
-function create() {
-
-  const config = {
-  type: Phaser.AUTO,
-  width: 800,
-  height: 600,
-  scene: {
     create: function() {
       this.add.text(100, 100, 'Hello Phaser!', { fontSize: '32px', fill: '#fff' });
     }
@@ -36,8 +10,3 @@ function create() {
 };
 
 const game = new Phaser.Game(config);
-
-}
-
-  // อัปเดตเกม
-}
