96070df9b55b9a2a121862e83010c71d593bd6b2
fuchiwara <Nichakorn_ronram@kkumail.com>
2025-03-19 21:57:21 +0700

    game.js

--- game.js
+++ game.js
@@ -5,6 +5,17 @@ const config = {
   scene: {
     create: function() {
       this.add.text(100, 100, 'Hello Phaser!', { fontSize: '32px', fill: '#fff' });
+      function preload() {
+  this.load.image('background', 'assets/background.png');
+        function create() {
+  this.add.image(400, 300, 'background');
+  this.add.text(100, 100, 'Hello Phaser!', { fontSize: '32px', fill: '#fff' });
+          function update() {
+  // โค้ดสำหรับอัปเดตเกมในทุกเฟรม
+}
+
+}
+
     }
   }
 };
